2cd52c404c2a8f32b2cd5d51cd653fcebb07bf7b
Weslley S. Patrocinio <wespatrocinio@gmail.com>
2016-10-24 23:12:22 -0200

    Advancing on join numpy arrays with IDs

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from pyspark import SparkConf, SparkContext, sql
 
 from settings import *
 from data.load import load_dataframe
+from recommender.datarep import *
 
 conf = SparkConf().setAppName('RecommenderWithSpark')\
 	   .set("spark.sql.pivotMaxValues", 500000)\
@@ -10,3 +11,7 @@ conf = SparkConf().setAppName('RecommenderWithSpark')\
 	   .set('spark.driver.memory','8g')
 sc = SparkContext(conf=conf)
 sql_context = sql.SQLContext(sc)
+
+df = load_dataframe("../data/user-product_map_reduced.json", sql_context)
+df = aggregate_count_by_user_id(df)
+df = generate_collaborative_matrix(df)
--- src/recommender/datarep.py
+++ src/recommender/datarep.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
-from pyspark.mllib.linalg.distributed import CoordinateMatrix, MatrixEntry
-
 def aggregate_count_by_user_id(df):
 	return df.groupBy('product_id', 'user_id').count()
 
 def generate_collaborative_matrix(df):
-	df_pvt = df.groupBy('product_id').pivot('product_id').sum('count')
+	return df.groupBy('product_id').pivot('product_id').sum('count').fillna(0)
+
+def join_pair(pair):
+    return dict(pair[0].asDict().items() + [("array", pair[1])])
+
+def add_products_arrays(df):
+	products_cols = df.columns
+	products_cols.remove('product_id')
+	df_array = df.select('product_id').rdd.zip(products_arrays.rdd).map(join_pair).toDF()
